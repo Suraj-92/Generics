af73d53361a9078861bf2652b13fe359f1fb50d6
Suraj-92 <suraj.temkar.92@gmail.com>
2021-02-05 18:17:03 +0530

    Given Three Float Value Find the maximum value UC-2 Done

--- src/main/java/com/Maximum.java
+++ src/main/java/com/Maximum.java
@@ -14,4 +14,16 @@ class Maximum{
         }
         return max;
     }
+    public Float testMaximum1(  Float x, Float y, Float z) {
+        Float max = x;
+        if(y.compareTo(max)>0)
+        {
+            max = y;
+        }
+        if (z.compareTo(max)>0)
+        {
+            max = z;
+        }
+        return max;
+    }
 }
--- src/test/java/com/MaximumTest.java
+++ src/test/java/com/MaximumTest.java
@@ -25,4 +25,26 @@ public class MaximumTest {
         maximum.testMaximum(xInt, yInt, zInt);
         Assertions.assertEquals(8,5);
     }
+    @Test
+    public void firstPositionFloatIsMaximum(){
+        Maximum maximum = new Maximum();
+        float xFloat = 4.3f;
+        float yFloat = 8.8f;
+        float zFloat = 5.5f;
+        maximum.testMaximum1(xFloat, yFloat, zFloat);
+        Assertions.assertEquals(8.8f,4.3f);
+    } @Test
+    public void secondPositionFloatIsMaximum(){
+        Maximum maximum = new Maximum();
+        Float xFloat = 4.3f, yFloat = 8.8f, zFloat = 5.5f;
+        maximum.testMaximum1(xFloat, yFloat, zFloat);
+        Assertions.assertEquals(8.8f,8.8f);
+    } @Test
+    public void thirdPositionFloatIsMaximum(){
+        Maximum maximum = new Maximum();
+        Float xFloat = 4.3f, yFloat = 8.8f, zFloat = 5.5f;
+        maximum.testMaximum1(xFloat, yFloat, zFloat);
+        Assertions.assertEquals(8.8f,5.5f);
+    }
+
 }
